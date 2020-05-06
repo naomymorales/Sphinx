f5d0952497be1b77f9d59e055c1a7cce348e948a
AngelHernandezDenis <adh3213@gmail.com>
2020-05-06 17:40:08 -0400

    New functions added

--- Math_Functions.py
+++ Math_Functions.py
@@ -14,8 +14,19 @@ def Polar_To_Cartersian(r_Coordenate, theta_Coordenate):
     y = r * math.sin(theta)
     return x, y
 
+def Degrees_To_Radians(degrees):
+    radians = degrees * (math.pi/180)
+    return radians
+
+def Radians_To_Degrees(radians):
+    degrees = radians * (180/math.pi)
+    return degrees
+
 def Magnitude(x, y):
     return math.sqrt((x * x) + (y * y))
 
 def Magnitude_With_3_Variables(x, y, z):
     return math.sqrt((x * x) + (y * y) + (z * z))
+
+def Distance_Between_Two_Points(x1, y1, x2, y2):
+    return math.sqrt(math.pow((x2 - x1), 2) + math.pow((y2 - y1), 2))
