259a664d9d4b64346bb7905eaa2b5b87e18fdbe4
AnelMartinezGomez <anel.martinez1@upr.edu>
2020-05-06 21:15:08 -0400

    Changed parser, in progress of adding correct tokens

--- Sphinx_Parser.py
+++ Sphinx_Parser.py
@@ -1,5 +1,5 @@
 from lark import Lark
-#testing import
+
 from Sphinx_Lexer import tokens
 #import ply.yacc as yacc
 import Sphinx_Lexer as Sphinx_Lexer
@@ -9,163 +9,116 @@ import Sphinx_Lexer as Sphinx_Lexer
 #commit try
 #PYTHONS GRAMMAR:  https://docs.python.org/3/reference/grammar.html
 
+
+
+
 Parser_Rules=Lark('''
-              character: /[a-zA-Z_]/|"?"|"_"
-              digit: /[0-9]/
-              delimiters: "("|")"|","|";"|"["|"]"
-              operators: ":="|"+"|"-"|"~"|"*"|"/"|"="|">="|"&"|"|"|"!="|"<"|">"|"<="
-              
-              
-              single_input: NEWLINE | simple_stmt | compound_stmt NEWLINE
-              file_input: (NEWLINE | stmt)* ENDMARKER
-              eval_input: testlist NEWLINE* ENDMARKER
-
-              decorator: "@" dotted_name [ "(" [arglist] ")" ] NEWLINE
-              decorators: decorator+
-              decorated: decorators (classdef | funcdef | async_funcdef)
-
-              async_funcdef: ASYNC funcdef
-              funcdef: "def" NAME parameters ["->" test] ":" [TYPE_COMMENT] func_body_suite
-
-              parameters: "(" [typedargslist] ")"
-              
-              
-              typedargslist: ((tfpdef ["=" test] ("," [TYPE_COMMENT] tfpdef ["=" test])* "," [TYPE_COMMENT] "/" ["," [ [TYPE_COMMENT] tfpdef ["=" test] 
-              ( "," [TYPE_COMMENT] tfpdef ["=" test] )* (TYPE_COMMENT
-                | ["," [TYPE_COMMENT] ["*" [tfpdef] ("," [TYPE_COMMENT] tfpdef ["=" test])* (TYPE_COMMENT
-                | ["," [TYPE_COMMENT] ["**" tfpdef [","] [TYPE_COMMENT]]])
-                | "**" tfpdef [","] [TYPE_COMMENT]]])
-                | "*" [tfpdef] ("," [TYPE_COMMENT] tfpdef ["=" test])* (TYPE_COMMENT 
-                | ["," [TYPE_COMMENT] ["**" tfpdef [","] [TYPE_COMMENT]]])
-                | "**" tfpdef [","] [TYPE_COMMENT]]] )
-                |  (tfpdef ["=" test] ("," [TYPE_COMMENT] tfpdef ["=" test])* (TYPE_COMMENT 
-                | ["," [TYPE_COMMENT] ["*" [tfpdef] ("," [TYPE_COMMENT] tfpdef ["=" test])* (TYPE_COMMENT 
-                | ["," [TYPE_COMMENT] ["**" tfpdef [","] [TYPE_COMMENT]]])
-                | "**" tfpdef [","] [TYPE_COMMENT]]])
-                | "*" [tfpdef] ("," [TYPE_COMMENT] tfpdef ["=" test])* (TYPE_COMMENT)
-                | ["," [TYPE_COMMENT] ["**" tfpdef [","] [TYPE_COMMENT]]])
-                | "**" tfpdef [","] [TYPE_COMMENT])
-                )
-              tfpdef: NAME [":" test]
-              
-              
-              
-              
-              varargslist: vfpdef ["=" test ]("," vfpdef ["=" test])* "," "/" ["," [ (vfpdef ["=" test] ("," vfpdef ["=" test])* 
-              ["," ["*" [vfpdef] ("," vfpdef ["=" test])* ["," ["**" vfpdef [","]]]
-               | "**" vfpdef [","]]]  
-               | "*" [vfpdef] ("," vfpdef ["=" test])* ["," ["**" vfpdef [","]]] 
-               | "**" vfpdef [","]) ]] 
-               | (vfpdef ["=" test] ("," vfpdef ["=" test])* ["," ["*" [vfpdef] ("," vfpdef ["=" test])* ["," ["**" vfpdef [","]]]
-               | "**" vfpdef [","]]]
-               | "*" [vfpdef] ("," vfpdef ["=" test])* ["," ["**" vfpdef [","]]]
-               | "**" vfpdef [","]
-              )
-              vfpdef: NAME
-              
-              
-              stmt: simple_stmt | compound_stmt
-              simple_stmt: small_stmt (";" small_stmt)* [";"] NEWLINE
-              small_stmt: (expr_stmt | del_stmt| pass_stmt| flow_stmt| import_stmt| global_stmt| nonlocal_stmt| assert_stmt)
-              expr_stmt: testlist_star_expr (annassign | augassign (yield_expr|testlist)| [("=" (yield_expr|testlist_star_expr))+ [TYPE_COMMENT]] )
-              annassign: ":" test ["=" (yield_expr |testlist_star_expr)]
-              testlist_star_expr: (test|star_expr) ("," (test|star_expr))* [","]
-              augassign: ("+=" | "-=" | "*=" | "@=" | "/=" | "%=" | "&=" | "|=" | "^=" |"<<=" | ">>=" | "**=" | "//=")
-              
-              
-              del_stmt: "del" exprlist
-              pass_stmt: "pass"
-              flow_stmt: break_stmt | continue_stmt | return_stmt | raise_stmt | yield_stmt
-              break_stmt: "break"
-              continue_stmt: "continue"
-              return_stmt: "return" [testlist_star_expr]
-              yield_stmt: yield_expr
-              raise_stmt: "raise" [test ["from" test]]
-              import_stmt: import_name | import_from
-              import_name: "import" dotted_as_names
-              
-              
-              
-              import_from: ("from" (("." | "...")* dotted_name | ("." | "...")+)"import" ("*" | "(" import_as_names ")" | import_as_names))
-              import_as_name: NAME ["as" NAME]
-              dotted_as_name: dotted_name ["as" NAME]
-              import_as_names: import_as_name ("," import_as_name)* [","]
-              dotted_as_names: dotted_as_name ("," dotted_as_name)*
-              dotted_name: NAME ("." NAME)*
-              global_stmt: 'global' NAME ("," NAME)*
-              nonlocal_stmt: "nonlocal" NAME ("," NAME)*
-              assert_stmt: "assert" test ["," test]
-
-              compound_stmt: if_stmt | while_stmt | for_stmt | try_stmt | with_stmt | funcdef | classdef | decorated | async_stmt
-              async_stmt: ASYNC (funcdef | with_stmt | for_stmt)
-              if_stmt: "if" namedexpr_test ":" suite ("elif" namedexpr_test ":" suite)* ["else" ":" suite]
-              while_stmt: "while" namedexpr_test ":" suite ["else" ":" suite]
-              for_stmt: "for" exprlist "in" testlist ":" [TYPE_COMMENT] suite ["else" ":" suite]
-              try_stmt: ("try" ":" suite((except_clause ":" suite)+["else" ":" suite]["finally" ":" suite]|"finally" ":" suite))
-              with_stmt: "with" with_item ("," with_item)*  ":" [TYPE_COMMENT] suite
-              with_item: test ["as" expr]
-              
-              
-              except_clause: "except" [test ["as" NAME]]
-              suite: simple_stmt | NEWLINE INDENT stmt+ DEDENT
-
-              namedexpr_test: test [":=" test]
-              test: or_test ["if" or_test "else" test] | lambdef
-              test_nocond: or_test | lambdef_nocond
-              lambdef: "lambda" [varargslist] ":" test
-              lambdef_nocond: "lambda" [varargslist] ":" test_nocond
-              or_test: and_test ("or" and_test)*
-              and_test: not_test ("and" not_test)*
-              not_test: "not" not_test | comparison
-              comparison: expr (comp_op expr)*
-              
-              
-              
-              comp_op: "<"|">"|"=="|">="|"<="|"<>"|"!="|"in"|"not" "in"|"is"|"is" "not"
-              star_expr: "*" expr
-              expr: xor_expr ("|" xor_expr)*
-              xor_expr: and_expr ("^" and_expr)*
-              and_expr: shift_expr ("&" shift_expr)*
-              shift_expr: arith_expr (("<<"|">>") arith_expr)*
-              arith_expr: term (("+"|"-") term)*
-              term: factor (("*"|"@"|"/"|"%"|"//") factor)*
-              factor: ("+"|"-"|"~") factor | power
-              power: atom_expr ["**" factor]
-              atom_expr: [AWAIT] atom trailer*
-              atom: ("(" [yield_expr|testlist_comp] ")"|"[" [testlist_comp] "]"|"{" [dictorsetmaker] "}"|NAME | NUMBER | STRING+ | "..." | "None" | "True" | "False")
-              testlist_comp: (namedexpr_test|star_expr) ( comp_for | ("," (namedexpr_test|star_expr))* [","] )
-              trailer: "(" [arglist] ")" | "[" subscriptlist "]" | "." NAME
-              subscriptlist: subscript ("," subscript)* [","]
-              subscript: test | [test] ":" [test] [sliceop]
-              sliceop: ":" [test]
-              exprlist: (expr|star_expr) ("," (expr|star_expr))* [","]
-              testlist: test ("," test)* [","]
-              dictorsetmaker: ( ((test ":" test | "**" expr)(comp_for | ("," (test ":" test | "**" expr))* [","])) |((test | star_expr)(comp_for | ("," (test | star_expr))* [","])) )
-
-              classdef: "class" NAME ["(" [arglist] ")"] ":" suite
-
-              arglist: argument ("," argument)*  [","]
-              
-              
-              
-              argument: ( test [comp_for] | test ":=" test | test "=" test | "**" test | "*" test )
-
-              comp_iter: comp_for | comp_if
-              sync_comp_for: "for" exprlist "in" or_test [comp_iter]
-              comp_for: [ASYNC] sync_comp_for
-              comp_if: "if" test_nocond [comp_iter]
-
-              encoding_decl: NAME
-
-              yield_expr: "yield" [yield_arg]
-              yield_arg: "from" test | testlist_star_expr
 
 
-              func_body_suite: simple_stmt | NEWLINE [TYPE_COMMENT NEWLINE] INDENT stmt+ DEDENT
+           single_input: NEWLINE | simple_stmt | compound_stmt NEWLINE
+           file_input: (NEWLINE | stmt)* ENDMARKER
+           eval_input: testlist NEWLINE* ENDMARKER
+           decorator: "@" dotted_name [ "(" [arglist] ")" ] NEWLINE
+           decorators: decorator+
+           decorated: decorators (classdef | funcdef)
+           funcdef: "def" NAME parameters ":" suite
+           parameters: "(" [varargslist] ")"
+           varargslist: ((fpdef ["=" test] ",")*
+              ("*" NAME ["," "**" NAME] | "**" NAME) |
+              fpdef ["=" test] ("," fpdef ["=" test])* [","])
+           fpdef: NAME | "(" fplist ")"
+           fplist: fpdef ("," fpdef)* [","]
+           stmt: simple_stmt | compound_stmt
+           simple_stmt: small_stmt (";" small_stmt)* [";"] NEWLINE
+           small_stmt: (expr_stmt | print_stmt  | del_stmt | pass_stmt | flow_stmt |import_stmt | global_stmt | exec_stmt | assert_stmt)
+           expr_stmt: testlist (augassign (yield_expr|testlist) |
+                     ("=" (yield_expr|testlist))*)
+           augassign: ("+=" | "-=" | "*=" | "/=" | "%=" | "&=" | "|=" | "^=" | "<<=" | ">>=" | "**=" | "//=")
+                     
+           print_stmt: "print" ( [ test ("," test)* [","] ] | ">>" test [ ("," test)+ [","] ] )
+           del_stmt: "del" exprlist
+           pass_stmt: "pass"
+           flow_stmt: break_stmt | continue_stmt | return_stmt | raise_stmt | yield_stmt
+           break_stmt: "break"
+           continue_stmt: "continue"
+           return_stmt: "return" [testlist]
+           yield_stmt: yield_expr
+           raise_stmt: "raise" [test ["," test ["," test]]]
+           import_stmt: import_name | import_from
+           import_name: "import" dotted_as_names
+           import_from: ("from" ("."* dotted_name | "."+) "import" ("*" | "(" import_as_names ")" | import_as_names))
+           import_as_name: NAME ["as" NAME]
+           dotted_as_name: dotted_name ["as" NAME]
+           import_as_names: import_as_name ("," import_as_name)* [","]
+           dotted_as_names: dotted_as_name ("," dotted_as_name)*
+           dotted_name: NAME ("." NAME)*
+           global_stmt: "global" NAME ("," NAME)*
+           exec_stmt: "exec" expr ["in" test ["," test]]
+           assert_stmt: "assert" test ["," test]
+             
+           compound_stmt: if_stmt | while_stmt | for_stmt | try_stmt | with_stmt | funcdef | classdef | decorated
+           if_stmt: "if" test ":" suite ("elif" test ":" suite)* ["else" ":" suite]
+           while_stmt: "while" test ":" suite ["else" ":" suite]
+           for_stmt: "for" exprlist "in" testlist ":" suite ["else" ":" suite]
+           try_stmt: ("try" ":" suite((except_clause ":" suite)+
+            ["else" ":" suite]
+            ["finally" ":" suite] |
+             "finally" ":" suite))
+           with_stmt: "with" with_item ("," with_item)*  ":" suite
+           with_item: test ["as" expr]              
+ 
+           except_clause: "except" [test [("as" | ",") test]]
+           suite: simple_stmt | NEWLINE INDENT stmt+ DEDENT
+           testlist_safe: old_test ["," old_test)+ [","]]
+           old_test: or_test | old_lambdef
+           old_lambdef: "lambda" [varargslist] ":" old_test
+           test: or_test ["if" or_test :"else" test] | lambdef
+           or_test: and_test ("or" and_test)*
+           and_test: not_test ("and" not_test)*
+           not_test: '"not" not_test | comparison
+           comparison: expr (comp_op expr)*
+           comp_op: "<"|">"|"=="|">="|"<="|"<>"|"!="|"in"|"not" "in"|"is"|"is" "not"
+           expr: xor_expr ("|" xor_expr)*
+           xor_expr: and_expr ("^" and_expr)*
+           and_expr: shift_expr ("&" shift_expr)*
+           shift_expr: arith_expr (("<<"|">>") arith_expr)*
+           arith_expr: term (("+"|"-") term)*
+           term: factor (("*"|"/"|"%"|"//") factor)*
+           factor: ("+"|"-"|"~") factor | power
+           power: atom trailer* ["**" factor]
+           atom: ("(" [yield_expr|testlist_comp] ")" |
+                     "[" [listmaker] "]" |
+                            "{" [dictorsetmaker] "}" |
+                                   "`" testlist1 "`" |
+                                          NAME | NUMBER | STRING+)
+                                          
+           listmaker: test ( list_for | ("," test)* [","] )
+           testlist_comp: test ( comp_for | ("," test)* [","] )
+           lambdef: "lambda" [varargslist] ":" test
+           trailer: "(" [arglist] ")" | "[" subscriptlist "]" | "." NAME
+           subscriptlist: subscript ("," subscript)* [","]
+           subscript: "." "." "." | test | [test] ":" [test] [sliceop]
+           sliceop: ":" [test]
+           exprlist: expr ("," expr)* [","]
+           testlist: test ("," test)* [","]
+           dictorsetmaker: ( (test ":" test (comp_for | ("," test ":" test)* [","])) |
+                  (test (comp_for | ("," test)* [","])) )
+           classdef: "clas" NAME ["(" [testlist] ")"] ":" suite
+           arglist: (argument ",")* (argument [","]
+                         |"*" test ("," argument)* ["," "**" test] 
+                         |"**" test)
+           argument: test [comp_for] | test "=" test
+           list_iter: list_for | list_if
+           list_for: "for" exprlist "in" testlist_safe [list_iter]
+           list_if: "if" old_test [list_iter]
+           comp_iter: comp_for | comp_if
+           comp_for: "for" exprlist 'in' or_test [comp_iter]
+           comp_if: "if" old_test [comp_iter]
+           testlist1: test ("," test)*
+           encoding_decl: NAME
+           yield_expr: "yield" [testlist]
+              
 
-              func_type_input: func_type NEWLINE* ENDMARKER
-              func_type: "(" [typelist] ")" "->" test
-              typelist: (test ("," test)* [","["*" [test] ("," test)* ["," "**" test] | "**" test]]|  "*" [test] ("," test)* ["," "**" test] | "**" test)
                       
               
               %import common.WS
