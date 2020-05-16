f49906c99455f750093089b2507cb93b7d17b624
naomy <naomy.morales@upr.edu>
2020-05-15 20:31:30 -0400

    Adding grammar rules

--- sphinx_lex_par.py
+++ sphinx_lex_par.py
@@ -114,4 +114,104 @@ def t_error(t):
 import ply.lex as lex
 
 lex.lex()
+# Parsing rules and precedence
+precedence = (
+    ('left', 'PLUS', 'MINUS'),
+    ('left', 'MULT', 'DIV'),
+    ('left', 'POWER'),
+    ('right', 'UMINUS'),
+)
+
+# dictionary to store the variable name and value
+names = {}
+
+
+def p_print_var_list(p):
+    '''statement : PRINTLIST '''
+    if len(names) is not 0:
+        for k, v in names.items():
+            print(k, "=", v)
+    else:
+        print("No variables are in the system.")
+
+
+def p_statement_assign(p):
+    '''statement : VAR EQUALS expression
+                 | VAR EQUALS equation
+                 | VAR EQUALS result'''
+    if p[3] is None:
+        pass
+    else:
+        names[p[1]] = p[3]
+
+
+def p_statement_expr(p):
+    '''statement : expression
+                 | equation
+                 | result'''
+    if p[1] is None:
+        pass
+    else:
+        print(p[1])
+
+
+def p_polynomial(p):
+    '''expression : expression PLUS expression
+                  | expression MINUS expression
+                  | expression MULT expression
+                  | expression DIV expression
+                  | expression POWER expression'''
+    p1 = str(p[1])
+    p3 = str(p[3])
+    if p[2] == '+' and p[1] is not None and p[3] is not None:
+        if "x" in p1 or "x" in p3:
+            p[0] = str(p[1]) + str(p[2]) + str(p[3])
+        else:
+            p[0] = p[1] + p[3]
+    elif p[2] == '-' and p[1] is not None and p[3] is not None:
+        if "x" in p1 or "x" in p3:
+            p[0] = str(p[1]) + str(p[2]) + str(p[3])
+        else:
+            p[0] = p[1] - p[3]
+    elif p[2] == '*' and p[1] is not None and p[3] is not None:
+        if "x" in p1 or "x" in p3:
+            p[0] = str(p[1]) + str(p[2]) + str(p[3])
+        else:
+            p[0] = p[1] * p[3]
+    elif p[2] == '/' and p[1] is not None and p[3] is not None:
+        if "x" in p1 or "x" in p3:
+            p[0] = str(p[1]) + str(p[2]) + str(p[3])
+        else:
+            p[0] = p[1] / p[3]
+    elif p[2] == '^' and p[1] is not None and p[3] is not None:
+        if "x" in p1 or "x" in p3:
+            p[0] = str(p[1]) + str(p[2]) + str(p[3])
+        else:
+            p[0] = math.pow(p[1], p[3])
+
+
+def p_quadratic(p):
+    '''expression : FLOAT CHARACTER POWER INT PLUS FLOAT CHARACTER PLUS FLOAT
+                  | FLOAT CHARACTER POWER INT PLUS FLOAT CHARACTER MINUS FLOAT
+                  | FLOAT CHARACTER POWER INT MINUS FLOAT CHARACTER PLUS FLOAT
+                  | FLOAT CHARACTER POWER INT MINUS FLOAT CHARACTER MINUS FLOAT'''
+    if p[4] == 2 and p[2] == p[7] and p[5] == '+' and p[8] == '+':
+        p[0] = str(p[1]) + str(p[2]) + str(p[3]) + str(p[4]) + str(p[5]) + str(p[6]) + str(p[7]) + str(p[8]) + str(p[9])
+    elif p[4] == 2 and p[2] == p[7] and p[5] == '+' and p[8] == '-':
+        p[0] = str(p[1]) + str(p[2]) + str(p[3]) + str(p[4]) + str(p[5]) + str(p[6]) + str(p[7]) + str(p[8]) + str(p[9])
+    elif p[4] == 2 and p[2] == p[7] and p[5] == '-' and p[8] == '+':
+        p[0] = str(p[1]) + str(p[2]) + str(p[3]) + str(p[4]) + str(p[5]) + str(p[6]) + str(p[7]) + str(p[8]) + str(p[9])
+    elif p[4] == 2 and p[2] == p[7] and p[5] == '-' and p[8] == '-':
+        p[0] = str(p[1]) + str(p[2]) + str(p[3]) + str(p[4]) + str(p[5]) + str(p[6]) + str(p[7]) + str(p[8]) + str(p[9])
+    else:
+        pass
+
+
+def p_coordinates_exp(p):
+    '''expression : LPAR FLOAT COMMA FLOAT RPAR'''
+    if p[2] is not None and p[4] is not None:
+        p[0] = str(p[1]) + str(p[2]) + str(p[3]) + str(p[4]) + str(p[5])
+    else:
+        pass
+
 
